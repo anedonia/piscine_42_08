47b724325b5675295a5c8a300c64b2e1a836280d
anedonia <80250570+anedonia@users.noreply.github.com>
2021-08-24 17:22:01 +0200

    Add files via upload

---
+++ c00retry_2/ex06/ft_print_comb2.c
@@ -0,0 +1,32 @@
+#include <unistd.h>
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+	return ;
+}
+
+void	ft_print_comb2(void)
+{
+	int	a;
+	int	b;
+
+	a = 0;
+	b = 1;
+	while (a < 99)
+	{
+		while (b <= 99)
+		{
+			ft_putchar((a / 10) + '0');
+			ft_putchar((a % 10) + '0');
+			ft_putchar(' ');
+			ft_putchar((b / 10) + '0');
+			ft_putchar((b % 10) + '0');
+			if (a != 98)
+				write(1, ", ", 2);
+			b++;
+		}
+		a++;
+		b = a + 1;
+	}
+}
